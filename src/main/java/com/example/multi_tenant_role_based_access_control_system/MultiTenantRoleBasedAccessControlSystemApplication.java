b424a59e86af44aedf19fc97b0633545da4748a2
shubhanginaik <shubhangi.naik83@gmail.com>
2025-02-04 21:43:06 +0200

    add first commit

---
+++ src/main/java/com/example/multi_tenant_role_based_access_control_system/MultiTenantRoleBasedAccessControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.multi_tenant_role_based_access_control_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantRoleBasedAccessControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantRoleBasedAccessControlSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multi_tenant_role_based_access_control_system/MultiTenantRoleBasedAccessControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multi_tenant_role_based_access_control_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantRoleBasedAccessControlSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
